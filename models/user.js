a8f171ed046dbb81c508386330512fe33b6fca1a
Sebato <sebastien.prudhomme-gateau@etu.umontpellier.fr>
2021-04-30 18:52:22 +0200

    finalisation compatibilité des formulaires

--- models/user.js
+++ models/user.js
@@ -9,14 +9,14 @@ const userSchema = new mongoose.Schema({
     },
     lastname: { type: String, required: true },
     firstname: { type: String, required: true },
-    //birthday: { type: Date, required: true },
+    birthday: { type: Date, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    /*role: {
+    role: {
         type: String,
         enum: ['Client', 'Fournisseur', 'Administrateur'],
         required: true
-    },*/
+    },
     job: { type: String, required: false }
 });
 
