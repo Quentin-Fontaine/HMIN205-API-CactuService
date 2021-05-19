5f7ca84d81a797d0f10bd39fdd404e2d795fdd45
Quentin-Fontaine <quentin.font974@gmail.com>
2021-05-19 12:29:56 +0200

    CHangement de port d'écoute pour le serveur en ligne

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-app.set('port', 8888);
+app.set('port', 3006);
 const server = http.createServer(app);
 
-server.listen(8888);
+server.listen(3006);
